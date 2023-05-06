d74f098e591ebc59e99ffe574840133048540a24
Kinsley Alexander <kinsleyalexander@Kinsleys-MacBook-Air.local>
2023-05-06 18:57:51 -0400

    added places view

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -12,7 +13,6 @@ app.get('/', (req, res) => {
 
 app.get('*', (req, res) => {
     res.render('error404')
-  })
-  
+})
 
 app.listen(process.env.PORT)
