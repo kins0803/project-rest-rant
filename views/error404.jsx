f8e22ddba9fd398965d9a672efc3490c3ea0f29c
Kinsley Alexander <kinsleyalexander@Kinsleys-MacBook-Air.local>
2023-05-06 20:03:46 -0400

    adding bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -5,8 +5,14 @@ function error404 () {
     return (
       <Def>
           <main>
-              <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
+            <h1>404: PAGE NOT FOUND</h1>
+            <p>Oops, sorry, we can't find this page!</p>
+            <div>
+                <img src="/images/cute-dog.jpg" alt="Cute Dog"/>
+                <div>
+                    Photo by <a href="https://unsplash.com/@escape_your_mind?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Michael G</a> on <a href="https://unsplash.com/photos/vpnRelMxPHU?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+            </div>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/bowl-of-vegetables.jpg" alt="Bowl of Vegetables"/>
+                    <div>
+                        Photo by <a href="https://unsplash.com/@annapelzer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anna Pelzer</a> on <a href="https://unsplash.com/photos/IGfIGP5ONV0?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
