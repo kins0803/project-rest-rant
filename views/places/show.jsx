14b27b4fb092b0b04c83068db5bcc41527243c54
Kinsley Alexander <kinsleyalexander@Kinsleys-MacBook-Air.local>
2023-05-28 16:29:38 -0400

    Show places updated.

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,26 +5,48 @@ function show (data) {
     return (
         <Def>
           <main>
-            <div>
-              <h1>{ data.place.name }</h1>
-              <h2>Rating</h2>
-              <p>Not Rated</p>
-              <h2>Description</h2>
-              <p>
-                Located in { data.place.city }, { data.place.state } and serving { data.place.cuisines }
-              </p>
-              <img src={data.place.pic} alt={data.place.name} />
+            <div className='row'>
+              <div className='col-sm-6'>
+                <img src={data.place.pic} alt={data.place.name} />
+                <h3>
+                  Located in { data.place.city }, { data.place.state }
+                </h3>
+              </div>
+              <div className='col-sm-6'>
+                <h1>{ data.place.name }</h1>
+                <h2>
+                  Rating
+                </h2>
+                <h3>
+                  Not Rated
+                </h3>
+                <h2>
+                  Description
+                </h2>
+                <h3>
+                  { data.place.showEstablished() }
+                </h3>
+                <h4>
+                  Serving { data.place.cuisines }
+                </h4>
+                <a href={`/places/${data.id}/edit`} className='btn btn-warning'> 
+                  Edit
+                </a>  
+                <form method='POST' action={`/places/${data.id}?_method=DELETE`}> 
+                  <button type='submit' className='btn btn-danger'>
+                    Delete
+                  </button>
+                </form>
+              </div>
+              <div>
+                <h2>
+                  Comments
+                </h2>
+                <h3>
+                  No comments yet!
+                </h3>
+              </div>
             </div>
-            <h2>Comments</h2>
-            <p>No comments yet!</p>
-            <a href={`/places/${data.id}/edit`} className='btn btn-warning'> 
-              Edit
-            </a>  
-            <form method='POST' action={`/places/${data.id}?_method=DELETE`}> 
-              <button type='submit' className='btn btn-danger'>
-                Delete
-              </button>
-            </form>
           </main>
         </Def>
     )
