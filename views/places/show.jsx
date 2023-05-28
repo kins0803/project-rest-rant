8a444aa2c7825de2d285d142d3df00174871c9ae
Kinsley Alexander <kinsleyalexander@Kinsleys-MacBook-Air.local>
2023-05-28 19:12:56 -0400

    Added comment section functionality.

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -57,10 +57,37 @@ function show (data) {
                   </button>
                 </form>
               </div>
-              <hr />
-              <h2>Comments</h2>
+            </div>
+            <hr />
+            <h2>Comments</h2>
+            <div>
               {comments}
             </div>
+            <hr />
+            <h2>Got Your Own Rant or Rave?</h2>
+            <form method='POST' action={`/places/${data.place.id}/comment`}>
+              <div className='row'>
+                <div className='form-group col-sm-12'>
+                  <label htmlFor='content'>Content</label>
+                  <textarea id='content' name='content' className='form-control'></textarea>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='form-group col-sm-4'>
+                  <label htmlFor='author'>Author</label>
+                  <input id='author' name='author' className='form-control'/>
+                </div>
+                <div className='form-group col-sm-4'>
+                  <label htmlFor='stars'>Star Rating</label>
+                  <input type='range' step='0.5' min='1' max='5' id='stars' name='stars' className='form-control' />
+                </div>
+                <div className='form-group col-sm-2'>
+                  <label htmlFor='rant'>Rant?</label>
+                  <input type='checkbox' id='rant' name='rant' className='form-control' />
+                </div>
+              </div>
+              <input type='submit' value='Add Comment' className='btn btn-primary' />
+            </form>
           </main>
         </Def>
     )
