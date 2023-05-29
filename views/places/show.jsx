b1b6e7f8690b1ce638950d9e0666b32a95be9e73
Kinsley Alexander <kinsleyalexander@Kinsleys-MacBook-Air.local>
2023-05-29 16:02:43 -0400

    Added star rating display

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,21 @@ function show (data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className='inactive'>
+      Not yet rated
+    </h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = sumRatings / data.place.comments.length
+    rating = (
+      <h3>
+        {Math.round(averageRating)} stars
+      </h3>
+    )
     comments = data.place.comments.map(c => {
       return (
         <div className='border'>
@@ -36,6 +50,7 @@ function show (data) {
                 <h2>
                   Rating
                 </h2>
+                {rating}
                 <h3>
                   Not Rated
                 </h3>
